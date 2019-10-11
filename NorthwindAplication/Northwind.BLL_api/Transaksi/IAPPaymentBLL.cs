47f774901e2f96036685115bdf9e2e56ef28120b
aerofanz <55221937+aerofanz@users.noreply.github.com>
2019-10-11 19:15:07 +0700

    github

---
+++ NorthwindAplication/Northwind.BLL_api/Transaksi/IAPPaymentBLL.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.BLL_api.Transaksi
+{
+    public interface IAPPaymentBLL
+    {
+
+    }
+}
---
+++ NorthwindAplication/Northwind.Model/Transaksi/APSupplier.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Model.Transaksi
+{
+    public class APSupplier
+    {
+        public string Supplierid { get; set; }
+        public string Suppliercompany { get; set; }
+        public string Group { get; set; }
+        public string Nocoa { get; set; }
+    }
+}
